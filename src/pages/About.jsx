8e78aaff4f3ec39317e0b3a3bcfc8dbe2401e4d1
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-10-06 02:27:35 +0530

    new things added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ export default function About() {
         
            <div className=" bg-gray-600 bg-opacity-40  mt-32 py-2 ">
             <h3 className='text-3xl text-center  font-bold text-indigo-950 ' >Our Services </h3>
-              <div className="flex  justify-between px-20 py-10   ">
+              <div className="flex  flex-wrap gap-10  justify-between px-20 py-10   ">
 <div className="flex flex-col gap-1 text-indigo-950 text-xl font-semibold"> 
       <h5 className='font-semibold '>We Provide</h5>
       <li>Roadmaps</li>
