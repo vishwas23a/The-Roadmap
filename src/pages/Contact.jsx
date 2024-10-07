7c665a0e1e1fa7ce00788e3f4529c0a8ffd1a6ad
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-10-07 22:14:22 +0530

    added backend

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Contact.css";
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Navbar from "../components/Navbar";
 export default function Contact() {
   const [visible, setVisible] = useState(true);
+  const [feedData,setFeedData]=useState([])
+  const [data,setData]=useState({
+    name:'',
+    email:'',
+    message:''
+  });
+
+  useEffect(()=>{
+    axios.get("http://localhost:2001/info")
+    .then((res)=>setFeedData(res.data))
+  })
+
+
+  const send=(e)=>{
+    e.preventDefault()
+    axios.post('http://localhost:2001/info',data)
+    .then(res=>{
+      console.log(res);
+      toast("Feedback sent")
+      setData(
+        {
+          name:'',
+          email:'',
+          message:''
+        
+        }
+      )
+
+      
+    })
+    console.log(data.name);
+    
+  }
+  const handleChange=(e)=>{
+    const{name,value}=e.target;
+    setData((prev)=>({
+      ...prev,
+      [name]:value
+    }))}
+
   return (
     <>
+
     <div className="w-full"><Navbar /></div>  
+   
       <div class="container px-4 mx-auto ">
         <div class="mx-auto mt-10 ">
+      
           <div class="max-w-lg mx-auto px-8 py-6 bg-gray-100 rounded-lg shadow-lg shadow-zinc-700">
             <div className="flex w-full justify-between">
+  
               <h2
                 onClick={() => {
                   setVisible(true);
@@ -32,17 +79,20 @@ export default function Contact() {
             {visible == true ? (
               <form>
                 <div class="mb-4">
-                  <label class="block text-gray-800 mb-1" for="name">
+                  <label class="block text-gray-800 mb-1" htmlFor="name">
                     Your Name
                   </label>
                   <input
                     class="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300"
                     placeholder="Enter your name"
                     type="text"
+                    name="name"
+                    value={data.name}
+                    onChange={handleChange}
                   />
                 </div>
                 <div class="mb-4">
-                  <label class="block text-gray-800 mb-1" for="email">
+                  <label class="block text-gray-800 mb-1" htmlFor="email">
                     Your Email
                   </label>
                   <input
@@ -51,10 +101,12 @@ export default function Contact() {
                     name="email"
                     id="email"
                     type="email"
+                    onChange={handleChange}
+                    value={data.email}
                   />
                 </div>
                 <div class="mb-4">
-                  <label class="block text-gray-800 mb-1" for="message">
+                  <label class="block text-gray-800 mb-1" htmlFor="message">
                     Your Message
                   </label>
                   <textarea
@@ -63,60 +115,34 @@ export default function Contact() {
                     placeholder="Enter your message"
                     name="message"
                     id="message"
+                    onChange={handleChange}
+                    value={data.message}
                   ></textarea>
                 </div>
                 <button
+                type="submit"
                   class="w-full bg-yellow-300 text-gray-800 py-2 px-4 rounded-lg hover:bg-yellow-400 transition duration-300"
-                  type="submit"
+                 onClick={send}
                 >
                   Send Message
                 </button>
+                <ToastContainer/>
               </form>
             ) : (
               <div className="h-[23rem] p-1  ">
-                <div className="bg-gray-200 rounded-lg p-4 h-full w-full overflow-auto ">
-                  <ol className="flex flex-col gap-10">
-                    <ul>
-                      <li>Name</li>
-                      <li>Email</li>
-                      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quasi totam ipsum neque excepturi, veritatis tempore sit voluptate! Fugit error quam distinctio praesentium perspiciatis explicabo, assumenda modi natus inventore ad!</li>
-                    </ul>
-                    <ul>
-                      <li>Name</li>
-                      <li>Email</li>
-                      <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat porro maxime odit repellendus omnis dolorem cumque assumenda doloremque ea totam, non veritatis ducimus dignissimos adipisci beatae tenetur voluptatibus velit consectetur.</li>
-                    </ul>
-                    <ul>
-                      <li>Name</li>
-                      <li>Email</li>
-                      <li>Message</li>
-                    </ul>
-                    <ul>
-                      <li>Name</li>
-                      <li>Email</li>
-                      <li>Message</li>
-                    </ul>{" "}
-                    <ul>
-                      <li>Name</li>
-                      <li>Email</li>
-                      <li>Message</li>
-                    </ul>{" "}
-                    <ul>
-                      <li>Name</li>
-                      <li>Email</li>
-                      <li>Message</li>
-                    </ul>{" "}
-                    <ul>
-                      <li>Name</li>
-                      <li>Email</li>
-                      <li>Message</li>
-                    </ul>{" "}
-                    <ul>
-                      <li>Name</li>
-                      <li>Email</li>
-                      <li>Message</li>
-                    </ul>
-                  </ol>
+                <div className=" rounded-lg p-4 h-full w-full overflow-auto ">
+                <ol className="flex flex-col gap-8 ">
+                  {
+                  feedData.map((value,index)=>(
+                  
+                      <ul key={index} className=" flex flex-col gap-3 bg-white p-3  shadow-amber-400 shadow-lg rounded-lg">
+                      <li className=" text-zinc-700"><span className="font-bold">Name :</span> {value.name}</li>
+                      <li className=" text-zinc-700"> <span className="font-bold" >Email : </span>{value.email}</li>
+                      <li className=" text-zinc-700"> <span className="font-bold">Message :</span> {value.message}</li>
+                      </ul>
+                   
+                  ))
+                 } </ol>
                 </div>
               </div>
             )}
@@ -126,3 +152,4 @@ export default function Contact() {
     </>
   );
 }
+
